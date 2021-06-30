c13fcc8c2e41199f1554e778df42253fa8864123
Huu Bao <lehuubao2909@gmail.com>
2021-06-30 11:43:38 +0700

    add file

---
+++ index.js
@@ -0,0 +1,40 @@
+//Bai1
+//alert("Hello gyu"); canh bao
+
+// var age =19; // khai bao bien so
+// var firstName = "Bao";//khai bao bien string
+// var lastName = " Le";
+// var online = true;//khai bao bien boolean
+
+// var a,b;
+
+// a=null;//khong cso gia tri
+// b=2211;
+
+// const huubao = "handsome";
+// var fullName = firstName + lastName;
+
+// console.log("Your age is "+age);
+// console.log("Hello",firstName);
+// console.log("Are you online",online)
+// console.log(a)
+// console.log(b)
+// console.log("My fullName is",fullName)
+// console.log(fullName +" is "+huubao)
+
+//Bai2
+var friend = 10;
+
+//friend = friend +1;
+//friend = friend -1;
+//friend = friend *2;
+//friend = friend/2;
+//friend = friend**2; //giai thua
+//var gruop = friend%3; //(=1) chia lay phan du
+
+//friend +=1;
+//friend -=1;
+//friend *=1;
+//friend /=1;
+
+console.log(friend);
